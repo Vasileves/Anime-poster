df09ad498485cfe76746ecb70c7e894aeabc488b
Vasileves <55274720+Vasileves@users.noreply.github.com>
2020-02-21 01:06:33 +0300

    Something works well
    ついに!
    I'm the sensei of Anime.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,93 @@
+
 let moveanim = anime({
-  targets: 'img',
-  translateX: anime.random(-100, 100),
-  translateY: anime.random(-100, 100),
+  targets: '#fig1',
+  translateX: anime.random(-350, 350),
+  translateY: anime.random(-350, 350),
+  scale: anime.random(-2, 2),
+  rotate: anime.random(-90, 90),
   duration: 2000,
-  easing: 'linear'
+  easing: 'linear',
+  loop: true
+});
+
+let moveanim2 = anime({
+  targets: '#fig2',
+  translateX: anime.random(-350, 350),
+  translateY: anime.random(-350, 350),
+  scale: anime.random(-2, 2),
+  rotate: anime.random(-90, 90),
+  duration: 2000,
+  easing: 'linear',
+  loop: true
+});
+
+let moveanim3 = anime({
+  targets: '#fig3',
+  translateX: anime.random(-350, 350),
+  translateY: anime.random(-350, 350),
+  scale: anime.random(-2, 2),
+  rotate: anime.random(-90, 90),
+  duration: 2000,
+  easing: 'linear',
+  loop: true
+
+});
+
+let moveanim4 = anime({
+  targets: '#fig4',
+  translateX: anime.random(-350, 350),
+  translateY: anime.random(-350, 350),
+  scale: anime.random(-2, 2),
+  rotate: anime.random(-90, 90),
+  duration: 2000,
+  easing: 'linear',
+  loop: true
+
+});
+
+let moveanim5 = anime({
+  targets: '#fig5',
+  translateX: anime.random(-350, 350),
+  translateY: anime.random(-350, 350),
+  scale: anime.random(-2, 2),
+  rotate: anime.random(-90, 90),
+  duration: 2000,
+  easing: 'linear',
+  loop: true
+
+});
+
+let moveanim6 = anime({
+  targets: '#fig6',
+  translateX: anime.random(-350, 350),
+  translateY: anime.random(-350, 350),
+  scale: anime.random(-2, 2),
+  rotate: anime.random(-90, 90),
+  duration: 2000,
+  easing: 'linear',
+  loop: true
+
+});
+
+let moveanim7 = anime({
+  targets: '#fig7',
+  translateX: anime.random(-350, 350),
+  translateY: anime.random(-350, 350),
+  scale: anime.random(-2, 2),
+  rotate: anime.random(-90, 90),
+  duration: 2000,
+  easing: 'linear',
+  loop: true
+
+});
+
+let moveanim8 = anime({
+  targets: '#fig8',
+  translateX: anime.random(-350, 350),
+  translateY: anime.random(-350, 350),
+  scale: anime.random(-2, 2),
+  rotate: anime.random(-90, 90),
+  duration: 2000,
+  easing: 'linear',
+  loop: true
 });
